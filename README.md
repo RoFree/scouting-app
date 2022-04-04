153c74b75972cf5220efb7bac7bbe18e711421b3
Ron <ron@Roberts-Mac-mini.local>
2022-04-03 18:39:04 -0700

    added api test

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A webapp for FRC scouting...
 
+I added a backend to the blue alliance api using a port of an npm library. This code is not deployable, I made every form field optional to speed up testing. In the form.js, I did a simple automation where it autogets the team name, and end game. To add more data points take a look at the score breakdown in the match data array, it has a lot of info. There is also support for arbitrary api calls, look at the library. There are also some values hardcoded (event code, year) that need to change. Also make sure to add an api key, you can get one by creating a blue alliance account.
+
 ## Backend
 
 The provided PHP backend is merely an example. You can use your own google form backend (read the MD file in /backend), use the provided PHP, or use your own system. In the PHP form, security is provided through the form key validation. I would use PHPLiteAdmin for access.
--- form.js
+++ form.js
@@ -1,3 +1,39 @@
+//add api data to form before submit
+
+async function submit(){
+    var form = document.getElementById("theForm");
+    var team = form.elements["entry.1638702746"].value;
+    var match_number = form.elements["entry.508602665"].value;
+    var tba = new BlueAlliance("YOURAPIKEY");
+    var event_id = "cada" //you can get this by looking at a url for a blue alliance event
+    var event_year = 2022 
+    var event = await tba.getEvent(event_id, event_year);
+    var match = await tba.getMatch(event, "q", match_number); //get match data as large array, assumes qual matches
+    if(!tba.isMatchDone(match)) return false; //check match has actually finished
+    var a = (match['alliances']["blue"]["team_keys"].includes("frc"+team)) ? "blue":"red"; //what color alliance
+    var num = match['alliances'][a]["team_keys"].indexOf("frc"+team);//index of robot in alliance, reminder js arrays are weird
+    addHidden(form, "barsdone", match["score_breakdown"][a]["endgameRobot"+num]);
+    var nickname  = await TrackEvent.getTeam(team); //get team nickname
+    addHidden(form, "entry.215295328", nickname);
+    return true; //submit form
+}
+
+
+function addHidden(theForm, key, value) {
+    // Create a hidden input element, and append it to the form:
+    var input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = key; 
+    input.value = value;
+    theForm.appendChild(input);
+}
+
+
+
+
+
+
+
 //missed lower
 function incValueMissedLower()
 {
